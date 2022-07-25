233815e0b3e6505c33bb8ee08b8a7494c7eb9d67
jonesjacklewis <jones.jack.lewis@gmail.com>
2022-07-25 14:57:13 +0100

    Read and write to files

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir); // Add this line of code
+Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles); // Add this line of code
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName){
     List<string> salesFiles = new List<string>();
@@ -28,3 +31,27 @@ IEnumerable<string> FindFiles(string folderName){
     return salesFiles;
 
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles){
+    double salesTotal = 0;
+
+    // READ FILES LOOP
+
+    // Loop over each file path in salesFiles
+    foreach(var file in salesFiles){
+
+        // Read the content of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field ot the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
